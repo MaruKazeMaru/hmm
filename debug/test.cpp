5908716d7a4daf5d9e389661881d50ed8a1e42be
MaruKazeMaru <kaze47111@gmail.com>
2023-07-16 22:40:24 +0900

    update markov_model

--- debug/test.cpp
+++ debug/test.cpp
@@ -48,53 +48,65 @@ int main (void){
     }
 
     float** transition_probs = new float*[node_num - 1];
+    bool** connected = new bool*[node_num - 1];
     for(int i = 0; i < node_num - 1; ++i){
         scanf("%s", line);
         char** temps = split_comma(line, node_num);
 
         transition_probs[i] = new float[node_num];
+        connected[i] = new bool[node_num];
         for(int j = 0; j < node_num; ++j){
             transition_probs[i][j] = atof(temps[j]);
             delete temps[j];
+            connected[i][j] = (transition_probs[i][j] > .0f);
         }
 
         delete temps;
     }
 
-    MarkovModel mm(symbol_num, node_num, transition_probs, obs_symbol_probss);
+    MarkovModel mm_original(symbol_num, node_num, transition_probs, obs_symbol_probss);
+    MarkovModel mm_unknown(symbol_num, node_num, connected);
 
     for(int i = 0; i < node_num - 2; ++i){
         delete transition_probs[i];
+        delete connected[i];
         delete obs_symbol_probss[i];
     }
     delete transition_probs[node_num - 2];
+    delete connected[node_num - 2];
     delete transition_probs;
+    delete connected;
     delete obs_symbol_probss;
 
-    int seq_size;
-    scanf("%d", &seq_size);
+    int seq_num;
+    scanf("%d", &seq_num);
 
-    int* seq = new int[seq_size];
-    for(int n = 0; n < seq_size; ++n){
-        scanf("%d", &seq[n]);
+    if(seq_num < 0)
+        return 1;
+
+    printf("create sequences\n");
+    int** seqs = new int*[seq_num];
+    int* seq_sizes = new int[seq_num];
+    for(int t = 0; t < seq_num; ++t){
+        seqs[t] = mm_original.create_random_sequence(seq_sizes + t);
+        printf("%d", seqs[t][0]);
+        for(int n = 1; n < seq_sizes[t]; ++n)
+            printf(" , %d", seqs[t][n]);
+        printf("\n");
     }
 
-    int** seqs = new int*[1];
-    int* seq_sizes = new int[1];
-    seqs[0] = seq;
-    seq_sizes[0] = seq_size;
-    printf("study start\n");
+    printf("\nstudy start\ndiff = \n");
     for(int i = 0; i < 30; ++i){
         float diff;
-        mm.study_once(1, seq_sizes, seqs, &diff);
-        printf("diff = %f\n", diff);
+        mm_unknown.study_once(seq_num, seq_sizes, seqs, &diff);
+        printf("%f\n", diff);
         //mm.print();
     }
     delete seq_sizes;
     delete seqs;
-    delete seq;
 
-    mm.print();
+    printf("\nend study\n");
+    mm_unknown.print();
 
 
     return 0;
